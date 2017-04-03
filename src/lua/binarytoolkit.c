12d6d854d56d28d6fa97a73e2456847d0092cd21
endeavor <endeavor@rainbowsandpwnies.com>
2017-04-02 20:42:09 -0400

    more work on lua wrapper

--- src/lua/binarytoolkit.c
+++ src/lua/binarytoolkit.c
@@ -5,19 +5,33 @@
 
 #include "arch/source/hsvm.h"
 #include "arch/target/amd64.h"
+#include "bt/jit.h"
 #include "container/buf.h"
 #include "container/memmap.h"
 #include "platform/hsvm.h"
 
 
 static const struct luaL_Reg lbt_lib_f [] = {
-    {"test", lbt_test},
-
-    {"buf",    lbt_buf_new},
-    {"memmap", lbt_memmap_new},
+    {"boper_constant", lbt_boper_constant},
+    {"boper_variable", lbt_boper_variable},
+    {"buf",            lbt_buf_new},
+    {"jit",            lbt_jit_new},
+    {"memmap",         lbt_memmap_new},
+    {"varstore",       lbt_varstore_new},
 
     {NULL, NULL}
 };
+
+
+static const struct luaL_Reg lbt_boper_m [] = {
+    {"__gc", lbt_boper_gc},
+    {"type", lbt_boper_type},
+    {"identifier", lbt_boper_identifier},
+    {"bits", lbt_boper_bits},
+    {"value", lbt_boper_value},
+    {NULL, NULL}
+};
+
 
 
 static const struct luaL_Reg lbt_buf_m [] = {
@@ -25,6 +39,12 @@ static const struct luaL_Reg lbt_buf_m [] = {
     {"length", lbt_buf_length},
     {"slice", lbt_buf_slice},
     {"get", lbt_buf_get},
+    {NULL, NULL}
+};
+
+
+static const struct luaL_Reg lbt_jit_m [] = {
+    {"__gc", lbt_jit_gc},
     {NULL, NULL}
 };
 
@@ -38,12 +58,22 @@ static const struct luaL_Reg lbt_memmap_m [] = {
 };
 
 
+static const struct luaL_Reg lbt_varstore_m [] = {
+    {"__gc", lbt_varstore_gc},
+    {NULL, NULL}
+};
+
+
 struct lbt_constant {
     const char * name;
-    union {
-        uint64_t value;
-        const void * ptr;
-    };
+    uint64_t value;
+};
+
+
+enum {
+    LBT_ARCH_SOURCE_HSVM,
+    LBT_ARCH_TARGET_AMD64,
+    LBT_PLATFORM_HSVM
 };
 
 
@@ -52,22 +82,40 @@ struct lbt_constant lbt_constants [] = {
     {"MEMMAP_W", .value=2},
     {"MEMMAP_X", .value=4},
     {"MEMMAP_NOFAIL", .value=1},
-    {"arch_source_hsvm", .ptr=&arch_source_hsvm},
-    {"arch_target_amd64", .ptr=&arch_target_amd64},
-    {"platform_hsvm", .ptr=&platform_hsvm},
+    {"ARCH_SOURCE_HSVM", .value=LBT_ARCH_SOURCE_HSVM},
+    {"ARCH_TARGET_AMD64", .value=LBT_ARCH_TARGET_AMD64},
+    {"PLATFORM_HSVM", .value=LBT_PLATFORM_HSVM},
     {NULL, .value=-1}
 };
 
 
 LUALIB_API int luaopen_binarytoolkit (lua_State * L) {
+    luaL_newmetatable(L, "lbt.boper");
+    luaL_setfuncs(L, lbt_boper_m, 0);
+    lua_pushstring(L, "__index");
+    lua_pushvalue(L, -2);
+    lua_settable(L, -3);
+
     luaL_newmetatable(L, "lbt.buf");
     luaL_setfuncs(L, lbt_buf_m, 0);
     lua_pushstring(L, "__index");
     lua_pushvalue(L, -2);
     lua_settable(L, -3);
 
+    luaL_newmetatable(L, "lbt.jit");
+    luaL_setfuncs(L, lbt_jit_m, 0);
+    lua_pushstring(L, "__index");
+    lua_pushvalue(L, -2);
+    lua_settable(L, -3);
+
     luaL_newmetatable(L, "lbt.memmap");
     luaL_setfuncs(L, lbt_memmap_m, 0);
+    lua_pushstring(L, "__index");
+    lua_pushvalue(L, -2);
+    lua_settable(L, -3);
+
+    luaL_newmetatable(L, "lbt.varstore");
+    luaL_setfuncs(L, lbt_varstore_m, 0);
     lua_pushstring(L, "__index");
     lua_pushvalue(L, -2);
     lua_settable(L, -3);
@@ -86,8 +134,85 @@ LUALIB_API int luaopen_binarytoolkit (lua_State * L) {
 
 
 
-int lbt_test (lua_State * L) {
-    printf("this is a test\n");
+
+
+/*******************************************************************************
+* boper
+*******************************************************************************/
+struct boper * lbt_boper_check (lua_State * L, int position) {
+    void ** userdata = (void **) luaL_checkudata(L, position, "lbt.boper");
+    luaL_argcheck(L, userdata != NULL, position, "lbt.boper expected");
+    return (struct boper *) *userdata;
+}
+
+
+int lbt_boper_absorb (lua_State * L, struct boper * boper_to_absorb) {
+    struct boper ** boper = lua_newuserdata(L, sizeof(struct boper *));
+    luaL_getmetatable(L, "lbt.boper");
+    lua_setmetatable(L, -2);
+
+    *boper = boper_to_absorb;
+
+    return 1;
+}
+
+
+int lbt_boper_constant (lua_State * L) {
+    uint64_t bits = luaL_checkinteger(L, 1);
+    uint64_t value = luaL_checkinteger(L, 2);
+
+    return lbt_boper_absorb(L, boper_constant(bits, value));
+}
+
+
+int lbt_boper_variable (lua_State * L) {
+    uint64_t bits = luaL_checkinteger(L, 1);
+    const char * identifier = luaL_checkstring(L, 2);
+
+    return lbt_boper_absorb(L, boper_variable(bits, identifier));
+}
+
+
+int lbt_boper_type (lua_State * L) {
+    struct boper * boper = lbt_boper_check(L, 1);
+
+    if (boper_type(boper) == BOPER_VARIABLE)
+        lua_pushstring(L, "variable");
+    else if (boper_type(boper) == BOPER_CONSTANT)
+        lua_pushstring(L, "contant");
+    else
+        luaL_error(L, "error on lua_boper_type, invalid return result");
+
+    return 1;
+}
+
+
+int lbt_boper_identifier (lua_State * L) {
+    struct boper * boper = lbt_boper_check(L, 1);
+
+    lua_pushstring(L, boper_identifier(boper));
+
+    return 1;
+}
+
+
+int lbt_boper_bits (lua_State * L) {
+    struct boper * boper = lbt_boper_check(L, 1);
+
+    lua_pushinteger(L, boper_bits(boper));
+
+    return 1;
+}
+
+
+int lbt_boper_value (lua_State * L) {
+    struct boper * boper = lbt_boper_check(L, 1);
+
+    if (boper_type(boper) != BOPER_CONSTANT)
+        luaL_error(L, "boper_value called on non-constant boper");
+
+    lua_pushinteger(L, boper_value(boper));
+
     return 1;
 }
 
@@ -115,7 +240,7 @@ struct buf * lbt_buf_check (lua_State * L, int position) {
 
 
 int lbt_buf_new (lua_State * L) {
-    uint64_t length = luaL_checkinteger(L, 0);
+    uint64_t length = luaL_checkinteger(L, 1);
 
     struct buf ** buf = lua_newuserdata(L, sizeof(struct buf *));
     luaL_getmetatable(L, "lbt.buf");
@@ -173,6 +298,73 @@ int lbt_buf_get (lua_State * L) {
     return 1;
 }
 
+
+
+/*******************************************************************************
+* jit
+*******************************************************************************/
+
+
+struct jit * lbt_jit_check (lua_State * L, int position) {
+    void ** userdata = (void **) luaL_checkudata(L, position, "lbt.jit");
+    luaL_argcheck(L, userdata != NULL, position, "lbt.jit expected");
+    return (struct jit *) *userdata;
+}
+
+
+int lbt_jit_new (lua_State * L) {
+    unsigned int lbt_source = luaL_checkinteger(L, 1);
+    unsigned int lbt_target = luaL_checkinteger(L, 2);
+    unsigned int lbt_platform = luaL_checkinteger(L, 3);
+
+    const struct arch_source * arch_source = NULL;
+    const struct arch_target * arch_target = NULL;
+    const struct platform * platform = NULL;
+
+    if (lbt_source == LBT_ARCH_SOURCE_HSVM)
+        arch_source = &arch_source_hsvm;
+
+    if (lbt_target == LBT_ARCH_TARGET_AMD64)
+        arch_target = &arch_target_amd64;
+
+    if (lbt_platform == LBT_PLATFORM_HSVM)
+        platform = &platform_hsvm;
+
+    if (arch_source == NULL)
+        luaL_error(L, "bad arch source");
+    else if (arch_target == NULL)
+        luaL_error(L, "bad arch target");
+    else if (platform == NULL)
+        luaL_error(L, "bad platform");
+
+    struct jit ** jit = lua_newuserdata(L, sizeof(struct jit *));
+    luaL_getmetatable(L, "lbt.jit");
+    lua_setmetatable(L, -2);
+
+    *jit = jit_create(arch_source, arch_target, platform);
+
+    return 1;
+}
+
+
+int lbt_jit_gc (lua_State * L) {
+    struct jit * jit = lbt_jit_check(L, 1);
+    ODEL(jit);
+    return 0;
+}
+
+
+int lbt_jit_execute (lua_State * L) {
+    struct jit * jit = lbt_jit_check(L, 1);
+    struct varstore * varstore = lbt_varstore_check(L, 2);
+    struct memmap * memmap = lbt_memmap_check(L, 3);
+
+    int result = jit_execute(jit, varstore, memmap);
+
+    lua_pushinteger(L, result);
+
+    return 1;
+}
 
 
 /*******************************************************************************
@@ -252,4 +444,37 @@ int lbt_memmap_get_u8 (lua_State * L) {
     lua_pushinteger(L, u8);
 
     return 1;
+}
+
+
+
+/*******************************************************************************
+* varstore
+*******************************************************************************/
+
+
+struct varstore * lbt_varstore_check (lua_State * L, int position) {
+    void ** userdata = (void **) luaL_checkudata(L, position, "lbt.varstore");
+    luaL_argcheck(L, userdata != NULL, position, "lbt.varstore expected");
+    return (struct varstore *) *userdata;
+}
+
+
+int lbt_varstore_new (lua_State * L) {
+    uint64_t length = luaL_checkinteger(L, 0);
+
+    struct varstore ** varstore = lua_newuserdata(L, sizeof(struct varstore *));
+    luaL_getmetatable(L, "lbt.varstore");
+    lua_setmetatable(L, -2);
+
+    *varstore = varstore_create(length);
+
+    return 1;
+}
+
+
+int lbt_varstore_gc (lua_State * L) {
+    struct varstore * varstore = lbt_varstore_check(L, 1);
+    ODEL(varstore);
+    return 0;
 }
--- src/lua/binarytoolkit.h
+++ src/lua/binarytoolkit.h
@@ -11,12 +11,30 @@ LUALIB_API int luaopen_binarytoolkit (lua_State * L);
 int lbt_test (lua_State * L);
 
 
+struct boper * lbt_boper_check (lua_State * L, int position);
+int lbt_boper_absorb   (lua_State * L, struct boper * boper_to_absorb);
+int lbt_boper_constant (lua_State * L);
+int lbt_boper_variable (lua_State * L);
+int lbt_boper_gc       (lua_State * L);
+int lbt_boper_type     (lua_State * L);
+int lbt_boper_identifier (lua_State * L);
+int lbt_boper_bits     (lua_State * L);
+int lbt_boper_value    (lua_State * L);
+
+
 struct buf * lbt_buf_check (lua_State * L, int position);
+int lbt_buf_absorb (lua_State * L, struct buf * buf_to_absorb);
 int lbt_buf_gc     (lua_State * L);
 int lbt_buf_new    (lua_State * L);
 int lbt_buf_length (lua_State * L);
 int lbt_buf_slice  (lua_State * L);
 int lbt_buf_get    (lua_State * L);
+
+
+struct jit * lbt_jit_check (lua_State * L, int position);
+int lbt_jit_new     (lua_State * L);
+int lbt_jit_gc      (lua_State * L);
+int lbt_jit_execute (lua_State * L);
 
 
 struct memmap * lbt_memmap_check (lua_State * L, int position);
@@ -40,5 +58,9 @@ int lbt_memmap_set_u32_be (lua_State * L);
 int lbt_memmap_set_u64_le (lua_State * L);
 int lbt_memmap_set_u64_be (lua_State * L);
 */
+
+struct varstore * lbt_varstore_check (lua_State * L, int position);
+int lbt_varstore_new (lua_State * L);
+int lbt_varstore_gc  (lua_State * L);
 
 #endif
